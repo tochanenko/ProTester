90ecd1a8136e8b6f563290cc0724702c773f8f14
volodya2000 <vovan.zhuk3.14@gmail.com>
2020-11-15 18:19:14 +0200

    If applied,this commit will create the base structure of protester project

---
+++ backend/src/main/java/ua/project/protester/ProtesterApplication.java
@@ -0,0 +1,13 @@
+package ua.project.protester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtesterApplication.class, args);
+	}
+
+}

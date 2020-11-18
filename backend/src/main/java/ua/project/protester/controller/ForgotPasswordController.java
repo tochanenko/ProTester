ccc81a862b006b608bfb17a32867b70880e66b6e
VadimDudka <niqz99@gmail.com>
2020-11-19 00:36:47 +0200

    Update ForgotPasswordController

--- backend/src/main/java/ua/project/protester/controller/ForgotPasswordController.java
+++ backend/src/main/java/ua/project/protester/controller/ForgotPasswordController.java
@@ -3,7 +3,10 @@ package ua.project.protester.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ua.project.protester.model.UserDto;
+import ua.project.protester.exception.InvalidPasswordResetTokenException;
+import ua.project.protester.exception.MailSendException;
+import ua.project.protester.exception.UserNotFoundException;
+import ua.project.protester.model.User;
 import ua.project.protester.service.ResetPasswordService;
 
 @RestController
@@ -14,20 +17,36 @@ public class ForgotPasswordController {
     private final ResetPasswordService resetPasswordService;
 
     @PostMapping
-    public ResponseEntity<?> resetPasswordRequest(@RequestBody UserDto user) {
-        resetPasswordService.processResetPasswordRequest(user);
-        return ResponseEntity.accepted().build();
+    public ResponseEntity<?> resetPasswordRequest(@RequestBody User user) {
+        try {
+            resetPasswordService.processResetPasswordRequest(user.getEmail());
+            return ResponseEntity.accepted().build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (MailSendException e) {
+            return ResponseEntity.status(502).build();
+        }
     }
 
     @GetMapping("/confirm-reset")
-    public ResponseEntity<String> validateResetToken(@RequestBody String tokenValue) {
-        String email = resetPasswordService.processTokenValidation(tokenValue);
-        return ResponseEntity.ok(email);
+    public ResponseEntity<String> validateResetToken(@RequestParam("t") String tokenValue) {
+        try {
+            String email = resetPasswordService.processTokenValidation(tokenValue);
+            return ResponseEntity.ok(email);
+        } catch (InvalidPasswordResetTokenException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/reset-password")
-    public ResponseEntity<?> resetPassword(@RequestBody UserDto user) {
-        resetPasswordService.processPasswordReset(user);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> resetPassword(@RequestBody User user) {
+        try {
+            resetPasswordService.processPasswordReset(user.getEmail(), user.getPassword());
+            return ResponseEntity.ok().build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (MailSendException e) {
+            return ResponseEntity.status(502).build();
+        }
     }
 }

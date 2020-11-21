dd3ceccb9aab8136bf7681c0b2027c7d1a797e8c
Vladislav <itochanenko@gmail.com>
2020-11-21 14:05:38 +0200

    Add Forgot Password Component

---
+++ frontend/src/main/protester-gui/src/app/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {PasswordService} from "../../services/password.service.ts.service";
+import {Router} from "@angular/router";
+import {StorageService} from "../../services/auth/storage.service";
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  recoveryForm: FormGroup;
+  submitted = false;
+  image = 'assets/logo.png';
+
+  constructor(private passwordService: PasswordService,
+              private router: Router,
+              private storageService: StorageService,
+              private formBuilder: FormBuilder
+
+  ) { }
+
+  ngOnInit(): void {
+    this.recoveryForm = this.formBuilder.group({
+      email: [null, [Validators.required]]
+    });
+  }
+
+  get f() {
+    return this.recoveryForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.recoveryForm.invalid) {
+      console.log('invalid');
+      return;
+    }
+
+    const recoveryResponse = {
+      email: this.f.email.value
+    }
+
+    this.passwordService.forgotPassword(recoveryResponse).subscribe(
+      email => {
+        this.router.navigateByUrl('/pending-password').then();
+      }
+    )
+  }
+
+}

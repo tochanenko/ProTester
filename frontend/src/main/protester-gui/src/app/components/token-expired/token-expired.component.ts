a832a59a902e93885cfa2acab04c5b2053e16d30
Vladislav <itochanenko@gmail.com>
2020-11-21 14:02:03 +0200

    Add Token Expired Component

---
+++ frontend/src/main/protester-gui/src/app/components/token-expired/token-expired.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-expired',
+  templateUrl: './token-expired.component.html',
+  styleUrls: ['./token-expired.component.css']
+})
+export class TokenExpiredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

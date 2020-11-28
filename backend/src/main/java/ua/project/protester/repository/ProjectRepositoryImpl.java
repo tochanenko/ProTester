67b33bda281f4275a7c6b3309cd9ab3476cfda10
Julia Borovets <juliaborovets2001@gmail.com>
2020-11-28 22:55:58 +0200

    add project repository implementation

--- backend/src/main/java/ua/project/protester/repository/ProjectRepositoryImpl.java
+++ backend/src/main/java/ua/project/protester/repository/ProjectRepositoryImpl.java
@@ -1,57 +1,150 @@
 package ua.project.protester.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ua.project.protester.model.Project;
 import ua.project.protester.model.ProjectDto;
 import ua.project.protester.utils.Pagination;
+import ua.project.protester.utils.ProjectDtoRowMapper;
+import ua.project.protester.utils.ProjectRowMapper;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProjectRepositoryImpl implements ProjectRepository{
+@RequiredArgsConstructor
+@PropertySource("classpath:queries/project.properties")
+public class ProjectRepositoryImpl implements ProjectRepository {
+
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
+    private final ProjectRowMapper projectRowMapper;
+
+    @Value("${CREATE_PROJECT}")
+    private String createProject;
+
+    @Value("${UPDATE_PROJECT}")
+    private String updateProject;
+
+    @Value("${UPDATE_PROJECT_STATUS}")
+    private String updateProjectStatus;
+
+    @Value("${GET_ALL_PROJECTS}")
+    private String getAllProjects;
+
+    @Value("${GET_ALL_PROJECTS_FILTERED}")
+    private String getAllProjectsFiltered;
+
+    @Value("${FIND_BY_ID}")
+    private String findById;
+
+    @Value("${FIND_COUNT_OF_RECORDS}")
+    private String findCountOdRecords;
+
+    @Value("${FIND_BY_LINK}")
+    private String findByLink;
+
+    @Value("${FIND_BY_NAME}")
+    private String findByName;
+
     @Override
     public void create(Project project) {
+        MapSqlParameterSource namedParams = new MapSqlParameterSource();
+        namedParams.addValue("project_name", project.getProjectName());
+        namedParams.addValue("project_website_link", project.getProjectWebsiteLink());
+        namedParams.addValue("project_active", project.getProjectActive());
+        namedParams.addValue("creator_id", project.getCreatorId());
 
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedJdbcTemplate.update(createProject, namedParams, keyHolder);
+
+        Integer id = (Integer) (keyHolder.getKeys().get("project_id"));
+        project.setProjectId(id.longValue());
     }
 
     @Override
     public void update(Project project) {
+        MapSqlParameterSource namedParams = new MapSqlParameterSource();
+        namedParams.addValue("project_id", project.getProjectId());
+        namedParams.addValue("project_name", project.getProjectName());
+        namedParams.addValue("project_website_link", project.getProjectWebsiteLink());
+        namedParams.addValue("project_active", project.getProjectActive());
 
+        namedJdbcTemplate.update(updateProject, namedParams);
     }
 
     @Override
     public void changeProjectStatus(Long id, Boolean isActive) {
+        MapSqlParameterSource namedParams = new MapSqlParameterSource();
+        namedParams.addValue("project_id", id);
+        namedParams.addValue("project_active", isActive);
 
+        namedJdbcTemplate.update(updateProjectStatus, namedParams);
     }
 
     @Override
     public List<ProjectDto> findAll(Pagination pagination) {
-        return null;
+        MapSqlParameterSource namedParams = new MapSqlParameterSource();
+        namedParams.addValue("pageSize", pagination.getPageSize());
+        namedParams.addValue("offset", pagination.getOffset());
+        namedParams.addValue("filterProjectName", pagination.getProjectName() + "%");
+
+        return namedJdbcTemplate.query(getAllProjects, namedParams, new ProjectDtoRowMapper());
     }
 
     @Override
     public List<ProjectDto> findAllFilteredByStatus(Pagination pagination) {
-        return null;
+        MapSqlParameterSource namedParams = new MapSqlParameterSource();
+        namedParams.addValue("pageSize", pagination.getPageSize());
+        namedParams.addValue("offset", pagination.getOffset());
+        namedParams.addValue("filterProjectName", pagination.getProjectName() + "%");
+        namedParams.addValue("projectActive", pagination.getProjectActive());
+
+        return namedJdbcTemplate.query(getAllProjectsFiltered, namedParams, new ProjectDtoRowMapper());
     }
 
     @Override
     public Optional<Project> findById(Long id) {
-        return Optional.empty();
+        try {
+            MapSqlParameterSource namedParams = new MapSqlParameterSource();
+            namedParams.addValue("project_id", id);
+            return Optional.ofNullable(namedJdbcTemplate.queryForObject(findById, namedParams, projectRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Long getCountOfAllProjects() {
-        return null;
+        return namedJdbcTemplate.queryForObject(findCountOdRecords, new MapSqlParameterSource(), Long.class);
     }
 
     @Override
     public Optional<Project> findProjectByWebsiteLink(String link) {
-        return Optional.empty();
+        try {
+            MapSqlParameterSource namedParams = new MapSqlParameterSource();
+            namedParams.addValue("project_website_link", link);
+            return Optional.ofNullable(namedJdbcTemplate.queryForObject(findByLink, namedParams, projectRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Project> findProjectByName(String name) {
-        return Optional.empty();
+        try {
+            MapSqlParameterSource namedParams = new MapSqlParameterSource();
+            namedParams.addValue("project_name", name);
+            return Optional.ofNullable(namedJdbcTemplate.queryForObject(findByName, namedParams, projectRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 }

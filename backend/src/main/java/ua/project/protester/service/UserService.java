7234903be3da70c9f37823b58eeee77b83ffc586
volodya2000 <vovan.zhuk3.14@gmail.com>
2020-11-29 21:03:11 +0200

    Resolve PMD VIOLATION FINALLY!!!!!!

--- backend/src/main/java/ua/project/protester/service/UserService.java
+++ backend/src/main/java/ua/project/protester/service/UserService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AnonymousAucthenticationToken;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;

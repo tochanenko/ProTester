f47a6bbdfdff2b7f08a98de97a318cfa8a010909
volodya2000 <vovan.zhuk3.14@gmail.com>
2020-11-29 20:41:50 +0200

    Resolve problems p.2

--- frontend/src/main/protester-gui/src/app/app.module.ts
+++ frontend/src/main/protester-gui/src/app/app.module.ts
@@ -14,28 +14,16 @@ import {MatIconRegistry} from '@angular/material/icon';
 import {MaterialModule} from "./services/material.module";
 import {HeaderComponent} from './components/header/header.component';
 import {ProfileComponent} from './components/profile/profile.component';
-<<<<<<< HEAD
-import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
-import {PendingPasswordComponent} from './components/pending-password/pending-password.component';
-import {ChangePasswordComponent} from './components/change-password/change-password.component';
-import {TokenExpiredComponent} from './components/token-expired/token-expired.component';
-=======
+
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { PendingPasswordComponent } from './components/pending-password/pending-password.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { TokenExpiredComponent } from './components/token-expired/token-expired.component';
 import { ManageActionComponent } from './components/manage-action/manage-action.component';
-import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator";
->>>>>>> ea499f547a4bb25c323a4e0cc8dd506d4df4dcf0
 import {ProjectMenuComponent} from './project/project-menu/project-menu.component';
 import {ProjectCreateComponent} from './project/project-create/project-create.component';
 import {ProjectListComponent} from './project/project-list/project-list.component';
 import {ProjectUpdateComponent} from './project/project-update/project-update.component';
-<<<<<<< HEAD
-import {ManageActionComponent} from "./components/manage-action/manage-action.component";
-=======
->>>>>>> ea499f547a4bb25c323a4e0cc8dd506d4df4dcf0
 
 @NgModule({
   declarations: [
@@ -48,19 +36,11 @@ import {ManageActionComponent} from "./components/manage-action/manage-action.co
     PendingPasswordComponent,
     ChangePasswordComponent,
     TokenExpiredComponent,
-<<<<<<< HEAD
     ProjectMenuComponent,
     ProjectCreateComponent,
     ProjectListComponent,
     ProjectUpdateComponent,
     ManageActionComponent
-=======
-    ManageActionComponent
-    ProjectMenuComponent,
-    ProjectCreateComponent,
-    ProjectListComponent,
-    ProjectUpdateComponent
->>>>>>> ea499f547a4bb25c323a4e0cc8dd506d4df4dcf0
   ],
   imports: [
     BrowserModule,

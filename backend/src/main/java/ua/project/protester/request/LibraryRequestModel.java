e8f258c08616cad25ad5d20bb53b0a580ec58b59
HermanSm23 <30389612+HermanSm23@users.noreply.github.com>
2020-12-03 23:04:03 +0200

    fix minor errors

--- backend/src/main/java/ua/project/protester/request/LibraryRequestModel.java
+++ backend/src/main/java/ua/project/protester/request/LibraryRequestModel.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import ua.project.protester.model.executable.ExecutableComponent;
 import ua.project.protester.model.executable.Step;
 
 import java.util.List;

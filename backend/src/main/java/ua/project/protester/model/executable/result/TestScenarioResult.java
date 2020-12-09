9ca576aac5f5765b4eaa1eec7308c03d4d7eeebc
VadimDudka <niqz99@gmail.com>
2020-12-09 12:46:12 +0200

    Add TestScenarioResult model

---
+++ backend/src/main/java/ua/project/protester/model/executable/result/TestScenarioResult.java
@@ -0,0 +1,8 @@
+package ua.project.protester.model.executable.result;
+
+import ua.project.protester.model.User;
+
+public class TestScenarioResult {
+    private Integer id;
+    private User startedByUser;
+}

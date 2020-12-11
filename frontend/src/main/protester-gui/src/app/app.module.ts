e27c5e61637ad7aa0d44aeba07635bdab378269f
HermanSm23 <30389612+HermanSm23@users.noreply.github.com>
2020-12-11 17:50:09 +0200

    optimize imports

--- frontend/src/main/protester-gui/src/app/app.module.ts
+++ frontend/src/main/protester-gui/src/app/app.module.ts
@@ -15,28 +15,28 @@ import {MaterialModule} from "./services/material.module";
 import {HeaderComponent} from './components/header/header.component';
 import {ProfileComponent} from './components/profile/profile.component';
 
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { PendingPasswordComponent } from './components/pending-password/pending-password.component';
-import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { TokenExpiredComponent } from './components/token-expired/token-expired.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {PendingPasswordComponent} from './components/pending-password/pending-password.component';
+import {ChangePasswordComponent} from './components/change-password/change-password.component';
+import {TokenExpiredComponent} from './components/token-expired/token-expired.component';
 import {ProjectMenuComponent} from './components/project/project-menu/project-menu.component';
 import {ProjectCreateComponent} from './components/project/project-create/project-create.component';
 import {ProjectListComponent} from './components/project/project-list/project-list.component';
 import {ProjectUpdateComponent} from './components/project/project-update/project-update.component';
-import { UsersListComponent } from './components/users-list/users-list.component';
+import {UsersListComponent} from './components/users-list/users-list.component';
 import {ActionsListComponent} from "./actions/actions-list/actions-list.component";
 import {ActionUpdateComponent} from "./actions/action-update/action-update.component";
 import {TestCaseListComponent} from "./test-case/test-case-list/test-case-list.component";
 import {TestCaseUpdateComponent} from "./test-case/test-case-update/test-case-update.component";
 import {TestCaseCreateComponent} from "./test-case/test-case-create/test-case-create.component";
 import {NgxMatSelectSearchModule} from "ngx-mat-select-search";
-import {DatasetListComponent} from "./dataset/dataset-list/dataset-list.component";
-import  {DatasetCreateComponent} from "./dataset/dataset-create/dataset-create.component";
 import {DatasetEditComponent} from "./dataset/dataset-edit/dataset-edit.component";
-import { DatasetDeleteComponent } from './dataset/dataset-delete/dataset-delete.component';
-import { DatasetViewComponent } from './dataset/dataset-view/dataset-view.component';
+import {DatasetCreateComponent} from "./dataset/dataset-create/dataset-create.component";
+import {DatasetListComponent} from "./dataset/dataset-list/dataset-list.component";
+import {DatasetViewComponent} from "./dataset/dataset-view/dataset-view.component";
+import {DatasetDeleteComponent} from "./dataset/dataset-delete/dataset-delete.component";
 import {ViewUserComponent} from "./components/view-user/view-user.component";
-import { EditUserComponent } from './components/edit-user/edit-user.component';
+import {EditUserComponent} from './components/edit-user/edit-user.component';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
@@ -60,27 +60,30 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
     TestCaseListComponent,
     TestCaseUpdateComponent,
     TestCaseCreateComponent,
+    ViewUserComponent,
+    EditUserComponent,
+    TestCaseCreateComponent,
     DatasetListComponent,
     DatasetCreateComponent,
     DatasetEditComponent,
     DatasetDeleteComponent,
-    DatasetViewComponent
+    DatasetViewComponent,
     TestCaseCreateComponent,
     ViewUserComponent,
     EditUserComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule,
-        CommonModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        MaterialModule,
-        NgxMatSelectSearchModule,
-        MatCheckboxModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgxMatSelectSearchModule,
+    MatCheckboxModule
+  ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}, MatIconRegistry],
   bootstrap: [AppComponent]
 })

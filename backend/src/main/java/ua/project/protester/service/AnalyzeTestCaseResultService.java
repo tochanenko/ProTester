0849045c510d4fdf37e47efd2b5555aea2e566f4
VadimDudka <niqz99@gmail.com>
2020-12-14 02:10:29 +0200

    Comment services
    Comment some services to avoid compilation errors. Should be uncommented in future

--- backend/src/main/java/ua/project/protester/service/AnalyzeTestCaseResultService.java
+++ backend/src/main/java/ua/project/protester/service/AnalyzeTestCaseResultService.java
@@ -2,7 +2,7 @@ package ua.project.protester.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ua.project.protester.exception.TestCaseResultNotFoundException;
+import ua.project.protester.exception.result.TestCaseResultNotFoundException;
 import ua.project.protester.model.executable.result.TestCaseResult;
 import ua.project.protester.repository.result.TestCaseResultRepository;
 
@@ -13,7 +13,9 @@ public class AnalyzeTestCaseResultService {
     private final TestCaseResultRepository testCaseResultRepository;
 
     public TestCaseResult getTestCaseResultById(int id) throws TestCaseResultNotFoundException {
-        return testCaseResultRepository.findTestCaseResultById(id)
-                    .orElseThrow(TestCaseResultNotFoundException::new);
+        // TODO: uncomment
+        //return testCaseResultRepository.findTestCaseResultById(id)
+        //            .orElseThrow(TestCaseResultNotFoundException::new);
+        return null;
     }
 }

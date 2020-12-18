64b45351508bc4d4148f4c6e5bfb6976ff1136ab
Egich <raidervolt@gmail.com>
2020-12-18 10:10:20 +0200

    Fixed bug with not closing bottom sheet.

--- frontend/src/main/protester-gui/src/app/modules/libraries-menu/libraries/create/create.component.ts
+++ frontend/src/main/protester-gui/src/app/modules/libraries-menu/libraries/create/create.component.ts
@@ -171,22 +171,26 @@ export class CreateComponent implements OnInit {
     });
   }
 
-  parseDescription(description: string) {
-    const regexp = new RegExp('(\\${\\w*})');
-    let splitted = description.split(regexp);
-    return splitted.map(sub_string => {
-      if (sub_string.includes("${")) {
-        return {
-          text: sub_string.replace('${', '').replace('}', ''),
-          input: true
+  parseDescription(description: string | Object) {
+    if (typeof description !== "object") {
+      const regexp = new RegExp('(\\${\\w*})');
+      let splitted = description.split(regexp);
+      return splitted.map(sub_string => {
+        if (sub_string.includes("${")) {
+          return {
+            text: sub_string.replace('${', '').replace('}', ''),
+            input: true
+          }
+        } else {
+          return {
+            text: sub_string,
+            input: false
+          }
         }
-      } else {
-        return {
-          text: sub_string,
-          input: false
-        }
-      }
-    })
+      })
+    } else {
+      return description;
+    }
   }
 
   ngOnDestroy(): void {

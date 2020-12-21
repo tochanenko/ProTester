72b27668596232160af28b318816889653008c59
Egich <raidervolt@gmail.com>
2020-12-21 16:30:15 +0200

    Changed structure of executable component - field "name" now contain description, field "description" now contain description with placeholders.

--- frontend/src/main/protester-gui/src/app/models/test-scenario.ts
+++ frontend/src/main/protester-gui/src/app/models/test-scenario.ts
@@ -2,8 +2,8 @@ import {Step} from "./step.model";
 
 export class TestScenario {
   id?: number;
-  name?: string;
-  description?: string | Object;
+  name?: string | Object;
+  description?: string;
   type?: string;
   parameterNames?: Array<string>;
   steps?: Array<Step>;

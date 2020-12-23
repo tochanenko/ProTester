0a3811281509b5e485407e7721bc89289958ff8a
VadimDudka <niqz99@gmail.com>
2020-12-23 16:39:51 +0200

    Delete unused method

--- backend/src/main/java/ua/project/protester/service/TestCaseResultService.java
+++ backend/src/main/java/ua/project/protester/service/TestCaseResultService.java
@@ -4,11 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.project.protester.exception.result.TestCaseResultNotFoundException;
-import ua.project.protester.model.executable.result.ResultStatus;
 import ua.project.protester.model.executable.result.TestCaseResultDto;
 import ua.project.protester.repository.result.TestCaseResultRepository;
-
-import java.time.OffsetDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +16,5 @@ public class TestCaseResultService {
     @Transactional
     public TestCaseResultDto getTestCaseResultById(int id) throws TestCaseResultNotFoundException {
         return testCaseResultRepository.findById(id);
-    }
-
-    public int finishTestCaseResultWithId(int id, ResultStatus finalStatus) {
-        return testCaseResultRepository.updateStatusAndEndDate(
-                id,
-                finalStatus,
-                OffsetDateTime.now());
     }
 }

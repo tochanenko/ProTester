069b85599019c1d75f19d7a9640975427cfd790f
HermanSm23 <30389612+HermanSm23@users.noreply.github.com>
2020-12-24 14:42:16 +0200

    delete exception

--- backend/src/main/java/ua/project/protester/exception/LibraryNotFoundException.java
+++ backend/src/main/java/ua/project/protester/exception/LibraryNotFoundException.java
@@ -1,11 +1,6 @@
 package ua.project.protester.exception;
 
 public class LibraryNotFoundException extends Exception {
-
-    public LibraryNotFoundException(int id) {
-        super(String.format("Library with id: %d not found", id));
-    }
-
     public LibraryNotFoundException(String message) {
         super(message);
     }

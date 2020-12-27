395cb8a918b3191ef46832207e15a1cd0dbebcde
Vladislav <itochanenko@gmail.com>
2020-12-27 03:12:38 +0200

    Fix users pages subscriptions

--- frontend/src/main/protester-gui/src/app/modules/shared/users/edit/edit.component.ts
+++ frontend/src/main/protester-gui/src/app/modules/shared/users/edit/edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {UserService} from "../../../../services/user/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {concatMap, switchMap} from "rxjs/operators";
 
 interface RoleView {
   value: string,
@@ -13,7 +14,7 @@ interface RoleView {
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
   editUserForm: FormGroup;
   selectedValue = 'ADMIN';
   isActive = true;
@@ -24,6 +25,8 @@ export class EditComponent implements OnInit {
   role = '';
   active = true;
   id = 0;
+
+  subscriptions = [];
 
   roles: RoleView[] = [
     {value: 'ADMIN', viewValue: 'Administrator'},
@@ -45,34 +48,60 @@ export class EditComponent implements OnInit {
       active: [this.active]
     });
 
-    this.route.params.subscribe(
-      params => {
+    this.subscriptions.push(this.route.params.pipe(
+      switchMap((params) => {
         this.id = params['id'];
-        this.userService.getUserById(this.id).subscribe(
-          user => {
+        return this.userService.getUserById(this.id);
+      })
+    ).subscribe(user => {
+      this.username = user['username'];
+      this.email = user['email'];
+      this.firstName = user['firstName'];
+      this.lastName = user['lastName'];
+      this.role = user['role'];
+      this.active = user['active'];
 
-            this.username = user['username'];
-            this.email = user['email'];
-            this.firstName = user['firstName'];
-            this.lastName = user['lastName'];
-            this.role = user['role'];
-            this.active = user['active'];
+      this.isActive = this.active;
+      this.selectedValue = this.role;
 
-            this.isActive = this.active;
-            this.selectedValue = this.role;
+      this.editUserForm = this.formBuilder.group({
+        username: [this.username],
+        email: [this.email],
+        firstName: [this.firstName],
+        lastName: [this.lastName],
+        role: this.role,
+        active: [this.active]
+      });
+    }));
 
-            this.editUserForm = this.formBuilder.group({
-              username: [this.username],
-              email: [this.email],
-              firstName: [this.firstName],
-              lastName: [this.lastName],
-              role: this.role,
-              active: [this.active]
-            });
-          }
-        );
-      }
-    );
+    // this.route.params.subscribe(
+    //   params => {
+    //     this.id = params['id'];
+    //     this.userService.getUserById(this.id).subscribe(
+    //       user => {
+    //
+    //         this.username = user['username'];
+    //         this.email = user['email'];
+    //         this.firstName = user['firstName'];
+    //         this.lastName = user['lastName'];
+    //         this.role = user['role'];
+    //         this.active = user['active'];
+    //
+    //         this.isActive = this.active;
+    //         this.selectedValue = this.role;
+    //
+    //         this.editUserForm = this.formBuilder.group({
+    //           username: [this.username],
+    //           email: [this.email],
+    //           firstName: [this.firstName],
+    //           lastName: [this.lastName],
+    //           role: this.role,
+    //           active: [this.active]
+    //         });
+    //       }
+    //     );
+    //   }
+    // );
   }
 
   ngOnInit(): void {
@@ -92,13 +121,17 @@ export class EditComponent implements OnInit {
       active: this.f.active.value
     };
 
-    this.userService.updateUser(this.id, editUserResponse).subscribe(
+    this.subscriptions.push(this.userService.updateUser(this.id, editUserResponse).subscribe(
       res => this.router.navigate([`/account/users/${this.id}`]),
       err => console.log(err)
-    );
+    ));
   }
 
   updateRole(event: string) {
     this.role = event;
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
--- frontend/src/main/protester-gui/src/app/modules/shared/users/view/view.component.ts
+++ frontend/src/main/protester-gui/src/app/modules/shared/users/view/view.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {User} from "../../../../models/user.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UserService} from "../../../../services/user/user.service";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewComponent implements OnInit {
+export class ViewComponent implements OnInit, OnDestroy {
   userId: number = -1;
   user: User = null;
 
   completed = false;
+
+  subscriptions = [];
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -29,11 +32,32 @@ export class ViewComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.userId = params['id'];
+    // this.route.params.subscribe(params => {
+    //   this.userId = params['id'];
+    //
+    //   this.userService.getUserById(this.userId).subscribe(
+    //     user => {
+    //       this.user.id = user['id'];
+    //       this.user.email = user['email'];
+    //       this.user.firstName = user['firstName'];
+    //       this.user.lastName = user['lastName'];
+    //       this.user.isActive = user['active'];
+    //       this.user.role = user['role'];
+    //       this.user.username = user['username'];
+    //
+    //       this.completed = true;
+    //     },
+    //     err => console.log(err)
+    //   )
+    // });
 
-      this.userService.getUserById(this.userId).subscribe(
-        user => {
+    this.subscriptions.push(
+      this.route.params.pipe(
+        switchMap(params => {
+          this.userId = params['id'];
+          return this.userService.getUserById(this.userId);
+        })
+      ).subscribe(user => {
           this.user.id = user['id'];
           this.user.email = user['email'];
           this.user.firstName = user['firstName'];
@@ -46,6 +70,10 @@ export class ViewComponent implements OnInit {
         },
         err => console.log(err)
       )
-    });
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe);
   }
 }
